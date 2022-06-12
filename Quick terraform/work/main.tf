66995f313d03da20e6856198929342c19a8918aa
Vittal111 <vittalbhandage111@gmail.com>
2022-06-12 18:35:18 +0530

    2nd commit

--- Quick terraform/work/main.tf
+++ Quick terraform/work/main.tf
@@ -42,5 +42,8 @@ resource "aws_subnet" "subnet2" {
 
 resource "aws_instance" "myec2" {
   instance_type = var.instance_type
-  ami = ami-0e449176cecc3e577
+  ami = "ami-0e449176cecc3e577"
+  count = 2
+  associate_public_ip_address = true
+
 }
