c5e00b3849a2935424da64a5b233b707f0f9a531
Vittal111 <vittalbhandage111@gmail.com>
2022-07-10 08:55:27 +0530

    final draft

--- Quick terraform/work/VPC/main.tf
+++ Quick terraform/work/VPC/main.tf
@@ -10,6 +10,13 @@ resource "aws_vpc" "myvpc" {
   cidr_block = var.cidr_block
 }
 
+resource "aws_internet_gateway" "mygateway" {
+  vpc_id = aws_vpc.myvpc.id
+  tags = {
+    "Name" = "main"
+  }
+}
+
 resource "aws_default_network_acl" "default" {
   default_network_acl_id = aws_vpc.myvpc.default_network_acl_id
 
@@ -29,4 +36,31 @@ resource "aws_default_network_acl" "default" {
     from_port  = 0
     to_port    = 0
   }
+}
+resource "aws_default_route_table" "route" {
+  default_route_table_id = aws_vpc.myvpc.default_route_table_id
+
+  route{
+    cidr_block = aws_vpc.myvpc.cidr_block
+    gateway_id = aws_internet_gateway.mygateway.id
+  }
+}
+
+resource "aws_security_group" "default" {
+  vpc_id = aws_vpc.myvpc.id
+
+  egress{
+    protocol = -1
+    from_port = 0
+    to_port = 0
+    self = true
+  }
+
+  ingress{
+    protocol = -1
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
 }
